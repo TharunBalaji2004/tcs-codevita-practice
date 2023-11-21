39af2f1144a19f813f39e54cb50d20d9ab296568
TharunBalaji2004 <tharunbalaji31@gmail.com>
2023-11-21 07:45:13 +0530

    Added Coin Distribution

--- CoinDistribution/CoinDistribution.java
+++ CoinDistribution/CoinDistribution.java
@@ -1,5 +1,22 @@
 package CoinDistribution;
 
+import java.util.Scanner;
+
 public class CoinDistribution {
-    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int i = 1;
+        while (i <= n) {
+            int f = (i - 4) / 5;
+            int o = 1;
+            if ((i - 5 * f) % 2 == 0) {
+                o = 2;
+            }
+            int t = (i - 5 * f - o) / 2;
+            System.out.println(i + " " + (f + t + o) + " " + f + " " + t + " " + o);
+            i += 1;
+        }
+        sc.close();
+    }
 }
---
+++ SortingBoxes/SortingBoxes.java
@@ -0,0 +1,7 @@
+package SortingBoxes;
+
+public class SortingBoxes {
+    public static void main(String[] args) {
+        
+    }
+}
